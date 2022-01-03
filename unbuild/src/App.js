46bc2daa5be40fe5c973e14f1f6a1a241e35f16e
Victor Luis <89362663+victorluismf@users.noreply.github.com>
2022-01-03 05:05:02 -0300

    Initial commit

---
+++ unbuild/src/App.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import './App.css'
+
+export default () => {
+  return (
+    <section>
+
+            {/*--------------------------------------*/}
+            {/*               HEADER                 */}
+            {/*--------------------------------------*/}
+
+
+      <div className="Header">
+        <a className="Logo">〄 Logo</a>
+          <div className="Botoes">
+            <a className="Botao1">Home</a>
+            <a className="Botao2">Grid</a>
+            <a className="Botao3">Text</a>
+            <a className="Botao4">Footer</a>
+          </div>
+          <div className="MenuToggle">
+            <div className="Toggle1"></div>
+            <div className="Toggle2"></div>
+            <div className="Toggle3"></div>
+          </div>
+      </div>
+
+
+            {/*--------------------------------------*/}
+            {/*            HERO SECTION              */}
+            {/*--------------------------------------*/}
+
+      <div className="HeroSection">
+        <a className="Titulo">RESPONSIVE</a>
+        <a className="SubTitulo">Praticando técnicas de responsividade!</a>
+        <img className="Background" src="https://victorluismf.github.io/responsive/bg.jpg" width="auto" height="auto"/>
+      </div>
+
+
+            {/*--------------------------------------*/}
+            {/*            GRID SECTION              */}
+            {/*--------------------------------------*/}
+
+      <div className="GridSection">
+        <a className="GridTitulo">GRID</a>
+          <div className="FullGrid">
+            <a className="Grid1">
+              <a className="TituloGrid1">Grid 1</a>
+              <a className="TextGrid1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid2">
+              <a className="TituloGrid2">Grid 2</a>
+              <a className="TextGrid2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid3">
+              <a className="TituloGrid3">Grid 3</a>
+              <a className="TextGrid3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid4">
+              <a className="TituloGrid4">Grid 4</a>
+              <a className="TextGrid4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid5">
+              <a className="TituloGrid5">Grid 5</a>
+              <a className="TextGrid5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid6">
+              <a className="TituloGrid6">Grid 6</a>
+              <a className="TextGrid6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid7">
+              <a className="TituloGrid7">Grid 7</a>
+              <a className="TextGrid7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+            <a className="Grid8">
+              <a className="TituloGrid8">Grid 8</a>
+              <a className="TextGrid8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</a>
+            </a>
+          </div>
+      </div>
+
+
+            {/*--------------------------------------*/}
+            {/*            TEXT SECTION              */}
+            {/*--------------------------------------*/}
+
+
+      <div className="TextSection">
+        <a className="TituloText">Text</a>
+        <a className="Text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam maecenas sed enim ut sem viverra aliquet eget. Risus pretium quam vulputate dignissim suspendisse in est. Egestas sed sed risus pretium quam vulputate dignissim. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Tortor at risus viverra adipiscing at. Vitae auctor eu augue ut lectus arcu bibendum. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Nibh tellus molestie nunc non blandit massa. Varius quam quisque id diam vel quam elementum. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam. Ante in nibh mauris cursus mattis molestie. Tincidunt praesent semper feugiat nibh sed pulvinar proin. Tempus imperdiet nulla malesuada pellentesque elit. Quis ipsum suspendisse ultrices gravida dictum fusce.
+
+Tincidunt eget nullam non nisi est sit. Nibh mauris cursus mattis molestie a. Feugiat nibh sed pulvinar proin gravida hendrerit. Cursus turpis massa tincidunt dui. Quisque id diam vel quam elementum pulvinar etiam non. Mauris augue neque gravida in fermentum et sollicitudin ac. Massa tempor nec feugiat nisl pretium fusce id velit. Et egestas quis ipsum suspendisse. Adipiscing elit duis tristique sollicitudin. Ut tellus elementum sagittis vitae et leo duis ut. Arcu odio ut sem nulla pharetra diam sit amet nisl. Sollicitudin aliquam ultrices sagittis orci. Vulputate ut pharetra sit amet. Felis eget velit aliquet sagittis id. Rhoncus aenean vel elit scelerisque. Nunc id cursus metus aliquam eleifend. Massa id neque aliquam vestibulum morbi blandit. Nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Placerat vestibulum lectus mauris ultrices eros in.
+
+Quis vel eros donec ac odio tempor orci. Arcu non odio euismod lacinia at quis. Leo a diam sollicitudin tempor id. Viverra justo nec ultrices dui sapien. Semper risus in hendrerit gravida rutrum quisque non. Egestas integer eget aliquet nibh. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Enim sed faucibus turpis in eu. Metus dictum at tempor commodo ullamcorper a lacus. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Nullam non nisi est sit amet facilisis magna. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Neque convallis a cras semper auctor.
+
+Amet consectetur adipiscing elit duis tristique. Id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Placerat duis ultricies lacus sed turpis tincidunt. Neque gravida in fermentum et sollicitudin ac. Erat imperdiet sed euismod nisi porta. Ultricies mi quis hendrerit dolor magna eget est lorem ipsum. Odio ut enim blandit volutpat. Consectetur a erat nam at lectus urna duis. Enim diam vulputate ut pharetra sit amet aliquam id diam. Cursus eget nunc scelerisque viverra mauris in. Habitant morbi tristique senectus et. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sed enim ut sem viverra aliquet eget sit.</a>
+      </div>
+
+            {/*--------------------------------------*/}
+            {/*                FOOTER                */}
+            {/*--------------------------------------*/}
+
+      <div className="Footer">
+        <div className="Icones">
+          <img className="Instagram" src="https://victorluismf.github.io/responsive/icon-instagram.png" width="auto" height="auto"/>
+          <img className="Linkedin" src="https://victorluismf.github.io/responsive/icon-linkedin.png" width="auto" height="auto"/>
+          <img className="Github" src="https://victorluismf.github.io/responsive/icon-github.png" width="auto" height="auto"/>
+        </div>
+        <a className="Copyright">Victor Luis © 2021</a>
+      </div>
+
+
+    </section>
+  )
+}
